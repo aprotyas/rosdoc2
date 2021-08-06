063d0a7f09b28f3616f2d9936b93331df65a047e
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-08-06 14:02:35 -0400

    Skip `doxygen_builder` init/build for python pkgs
    If the build type is `ament_python`, then the initialization/build
    methods for the `doxygen_builder` object can be skipped entirely, which
    is what this commit addresses.
    
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>

--- rosdoc2/verbs/build/builders/doxygen_builder.py
+++ rosdoc2/verbs/build/builders/doxygen_builder.py
@@ -77,6 +77,14 @@ class DoxygenBuilder(Builder):
 
         assert self.builder_type == 'doxygen'
 
+        # If the build type is `ament_python`, there is no reason
+        # to create a doxygen builder.
+        if self.build_context.build_type == 'ament_python':
+            logger.info(
+                f"The package build type is 'ament_python', hence the"
+                f"'{self.builder_type}' builder is not generated")
+            return None
+
         self.doxyfile = None
         self.extra_doxyfile_statements = []
         self.rosdoc2_doxyfile_statements = []
@@ -149,6 +157,13 @@ class DoxygenBuilder(Builder):
             logger.info(f"Using user specified Doxyfile at '{self.doxyfile}'.")
 
     def build(self, *, doc_build_folder, output_staging_directory):
+        # If the build type is `ament_python`, there is no reason to run doxygen.
+        if self.build_context.build_type == 'ament_python':
+            logger.info(
+                f"The package build type is 'ament_python', hence the"
+                f"'{self.builder_type}' builder is not invoked")
+            return None  # Explicitly generated no documentation.
+
         # If both doxyfile and doxyfile_content are None, that means there is
         # no reason to run doxygen.
         if self.doxyfile is None and self.doxyfile_content is None:
