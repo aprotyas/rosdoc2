e188b2db0e452b11a670da041701cc6609b4ecac
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-08-11 13:12:59 -0400

    Apply code review suggestions on `DoxygenBuilder`
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>
    
    Co-authored-by: Chris Lalancette <clalancette@openrobotics.org>

--- rosdoc2/verbs/build/builders/doxygen_builder.py
+++ rosdoc2/verbs/build/builders/doxygen_builder.py
@@ -79,11 +79,11 @@ class DoxygenBuilder(Builder):
 
         # If the build type is not `ament_cmake/cmake`, there is no reason
         # to create a doxygen builder.
-        if (self.build_context.build_type not in ['ament_cmake', 'cmake'] and
+        if (self.build_context.build_type not in ('ament_cmake', 'cmake') and
                 not self.build_context.run_doxygen):
             logger.info(
-                f"The package build type is not 'ament_cmake/cmake', hence the"
-                f"'{self.builder_type}' builder is not generated")
+                f"The package build type is not 'ament_cmake' or 'cmake', hence the"
+                f"'{self.builder_type}' builder was not configured")
             return None
 
         self.doxyfile = None
@@ -159,11 +159,11 @@ class DoxygenBuilder(Builder):
 
     def build(self, *, doc_build_folder, output_staging_directory):
         # If the build type is not 'ament_cmake/cmake', there is no reason to run doxygen.
-        if (self.build_context.build_type not in ['ament_cmake', 'cmake'] and
+        if (self.build_context.build_type not in ('ament_cmake', 'cmake') and
                 not self.build_context.run_doxygen):
             logger.info(
-                f"The package build type is not 'ament_cmake/cmake', hence the"
-                f"'{self.builder_type}' builder is not invoked")
+                f"The package build type is not 'ament_cmake' or 'cmake', hence the"
+                f"'{self.builder_type}' builder was not invoked")
             return None  # Explicitly generated no documentation.
 
         # If both doxyfile and doxyfile_content are None, that means there is
